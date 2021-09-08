fec92fb03c24bb5bbc2430d1d8a5d6b7616ad139
danding <33542476+tangchunwei@users.noreply.github.com>
2021-09-08 11:55:48 +0800

    #修改布局

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <Tutorial/>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    @select="handleSelect"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b"
+  >
+    <el-menu-item index="1">使用简介</el-menu-item>
+    <el-menu-item index="2">一键分析</el-menu-item>
+    <el-menu-item index="3">后台登录</el-menu-item>
+    <el-menu-item index="4">上传未知系统</el-menu-item
+    >
+  </el-menu>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
 </script>
